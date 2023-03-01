a5127a2b0de76635db7ea53eb4b7b4256a04a83e
kiman <annabelle.bosse>
2023-03-01 10:03:45 +0100

    sql

--- _sql/base.sql
+++ _sql/base.sql
@@ -2,13 +2,14 @@ CREATE DATABASE videogames;
 
 USE videogames;
 
-CREATE TABLE game (
-  id TINYINT UNSIGNED PRIMARY KEY,
-  title VARCHAR(255),
-  description TEXT,
-  release_date DATE,
-  poster VARCHAR(255),
-  price DECIMAL(5,2)
+CREATE TABLE game
+(
+    id TINYINT PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT,
+    release_date DATE,
+    poster VARCHAR(255),
+    price DECIMAL(5,2)
 );
 
 
